73e58ac512adbe625abace16ad2b4113e6377e1e
Eric Mays <emays1973@gmail.com>
2019-08-02 22:14:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push("Tupac")
+  array.push()
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift("Cardi B")
+  array.unshift()
 end
 
 def remove_element_from_end_of_array(array)
