919383fe2c34b2a156e8725bf9f7eb011ff731cb
Eric Mays <emays1973@gmail.com>
2019-08-02 22:17:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,27 +11,27 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift()
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  famous_people.pop()
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  famous_people.shift()
+  array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  famous_people[1]
+  array[i]
 end
 
 def retrieve_first_element_from_array(array)
-  famous_people[0]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  famous_people[-1]
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
